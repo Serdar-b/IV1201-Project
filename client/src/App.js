70f1d4cb76cdf9b236b0c4704f43e01ae2eada6f
Mangelol <90220730+Mangelol@users.noreply.github.com>
2024-02-14 13:56:02 +0100

    added automatic routing to /login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPresenter from './presenter/LoginPresenter';
 import Dashboard from './view/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPresenter />} />
           <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
         </Routes>
       </div>
     </Router>
