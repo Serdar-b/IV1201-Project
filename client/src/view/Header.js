8daf43de66ab3e27aa8051f66641463d4edc3fbe
Serdar-b <serdar-bulun@outlook.com>
2024-02-19 20:36:15 +0100

    When you logout you invalidate the user

--- client/src/view/Header.js
+++ client/src/view/Header.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import homeIcon from '../images/home.png';
 
 const Header = () => {
-    return (
-        <header>
-          <nav>
-            <div className="nav-content">
-                <Link to="/dashboard"><img src={homeIcon} alt="Home" className="home-icon" /></Link>
-                <Link to="/dashboard" className="header-link">Grönalund jobb</Link>
-                <Link to="/login" className="header-link">Logout</Link> {/* Hardcoded logout, replace later */}
-            </div>
-          </nav>
-        </header>
-      );
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate('/login'); 
+  }
+
+  return (
+      <header>
+        <nav>
+          <div className="nav-content">
+              <Link to="/dashboard"><img src={homeIcon} alt="Home" className="home-icon" /></Link>
+              <Link to="/dashboard" className="header-link">Grönalund jobb</Link>
+              <button onClick={handleLogout} className="header-link">Logout</button>
+          </div>
+        </nav>
+      </header>
+    );
 };
 
 export default Header;
