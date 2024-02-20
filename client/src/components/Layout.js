6504febc785a8929412e42d5869d63823a70bd1d
BisherS <104938300+BisherS@users.noreply.github.com>
2024-02-20 13:06:01 +0100

    fixing JSDoc for the components

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Header from '../view/Header';
 
+/**
+ * A layout component that wraps content with optional header display.
+ * @param {boolean} [props.showHeader=true] - Whether to display the header.
+ * @param {React.ReactNode} props.children - The children elements to be wrapped.
+ * @returns {React.ReactElement} The layout component.
+ */
+
 const Layout = ({ children, showHeader = true }) => {
     return (
         <div>
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
+
+/**
+ * A protected route component that ensures a user is authenticated before rendering children components.
+ * @param {React.ReactNode} props.children - The children elements to be rendered if the user is authenticated.
+ * @returns {React.ReactElement} The protected route component.
+ */
+
 const ProtectedRoute = ({ children }) => {
+  // State to hold the current user data, initialized from local storage
   const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('user')));
 
+  // Effect hook to update currentUser state if the user data in local storage changes
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem('user'));
     setCurrentUser(userData);
   }, []);
 
+  // Redirect to login page if no currentUser is found
   if (!currentUser) {
-    // If there's no user, redirect to login page
     console.log('No user found, redirecting to login...');
     return <Navigate to="/login" replace />;
   }
 
+  // If there's a user, render the "children" components
   console.log('User found:', currentUser.name);
-  return children; // If there's a user, render the "children" components
+  return children;
 };
 
 export default ProtectedRoute;
