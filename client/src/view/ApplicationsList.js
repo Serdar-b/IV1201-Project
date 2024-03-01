c878967b4153a68cfa6b18d89cb0168088c73f07
Serdar-b <serdar-bulun@outlook.com>
2024-03-01 18:50:46 +0100

    Language from database also translated

--- client/src/view/ApplicationsList.js
+++ client/src/view/ApplicationsList.js
@@ -47,6 +47,41 @@ const ApplicationsList = ({
     }
   };
 
+  const translateCompetences = (competencesString) => {
+    if (!competencesString || competencesString === "none") {   
+      return t("database.no_competences");
+    }
+  
+    const competencesArray = competencesString.split(', ');
+  
+    const translatedCompetences = competencesArray.map(competence => {
+      const parts = competence.split(' (');
+      const competenceName = parts[0];
+      let years = parts[1];
+  
+      if (years) {
+        years = years.slice(0, -6);
+        const translatedYears = t('database.years');
+        years = `${years} ${translatedYears})`;
+      }
+  
+      const translatedName = t(`database.${competenceName}`);
+      return `${translatedName} (${years}`;
+    });
+  
+    return translatedCompetences.join(', ');
+  };
+  
+  const translateAvailability = (availabilityString) => {
+    const periodsArray = availabilityString.split('; ');
+  
+    const translatedPeriods = periodsArray.map(period => {
+      return period.replace(' to ', ` ${t('database.to')} `);
+    });
+  
+    return translatedPeriods.join('; ');
+  };
+
   return (
     <div className="applications-list">
       <h2>{t('applications_list.all_applications')}</h2>
@@ -72,22 +107,22 @@ const ApplicationsList = ({
           currentApplications.map((app) => (
             <div className="application-card" key={app.person_id}>
               <div><strong>{t('applications_list.applicant')}:</strong> {app.name} {app.surname}</div>
-              <div><strong>{t('applications_list.competences')}:</strong> {app.competences_with_experience}</div>
-              <div><strong>{t('applications_list.availability')}:</strong> {app.availability_periods}</div>
+              <div>  <strong>{t('applications_list.competences')}:</strong> {translateCompetences(app.competences_with_experience)}</div>
+              <div><strong>{t('applications_list.availability')}:</strong> {translateAvailability(app.availability_periods)}</div>
               <div style={getStatusStyle(app.status)}>
-                <strong>{t('applications_list.status')}:</strong> {app.status ? app.status.charAt(0).toUpperCase() + app.status.slice(1) : t('applications_list.unknown')}
+                <strong>{t('applications_list.status')}:</strong> {app.status ?  t(`database.${app.status}`) : t('database.unknown')}
               </div>
               <Select
                 name="status"
                 options={[
-                  { value: 'unhandled', label: 'Unhandled' },
-                  { value: 'accepted', label: 'Accepted' },
-                  { value: 'rejected', label: 'Rejected' }
+                  { value: 'unhandled', label: t(`database.unhandled`) },
+                  { value: 'accepted', label: t(`database.accepted`) },
+                  { value: 'rejected', label: t(`database.rejected`) }
                 ]}
                 className="basic-multi-select"
                 classNamePrefix="select"
                 onChange={(selectedOption) => handleStatusChange(selectedOption, app.person_id, app.competence_id)}
-                value={app.status ? { value: app.status, label: app.status.charAt(0).toUpperCase() + app.status.slice(1) } : null}
+                value={app.status ? { value: app.status, label: t(`database.${app.status}`) } : null}
               />
             </div>
           ))
@@ -97,7 +132,7 @@ const ApplicationsList = ({
       </div>
       <div className="pagination">
         <button onClick={() => paginate(-1)} disabled={currentPage <= 0}>{t('applications_list.left')}</button>
-        <span>Page {currentPage + 1} of {totalPages}</span>
+        <span>{t("applications_list.page")} {currentPage + 1} {t("applications_list.of")} {totalPages}</span>
         <button onClick={() => paginate(1)} disabled={currentPage >= totalPages - 1}>{t('applications_list.right')}</button>
       </div>
     </div>
