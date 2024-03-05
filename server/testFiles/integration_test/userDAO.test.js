6580b7edd29de71ce491c596326afe29be8d7d01
ismailwedihaji <ismailwedihaji@gmail.com>
2024-03-05 17:13:03 +0100

    test updated

--- server/testFiles/integration_test/userDAO.test.js
+++ server/testFiles/integration_test/userDAO.test.js
@@ -8,40 +8,42 @@ describe("createUser", () => {
   let newUser;
   let client;
 
-  beforeAll(async () => {
-    newUser = {
-      name: "Test",
-      surname: "User",
-      pnr: "1234567890",
-      email: "testuser@example.com",
-      password: "securepassword",
-      username: "testuser",
-    };
-    client = await pool.connect();
-  });
+  // beforeAll(
+  //   async () => {
+  //   // newUser = {
+  //   //   name: "Test",
+  //   //   surname: "User",
+  //   //   pnr: "1234567890",
+  //   //   email: "testuser@example.com",
+  //   //   password: "securepassword",
+  //   //   username: "testuser",
+  //   // };
+  //   client = await pool.connect();
+  // });
 
-  afterAll(async () => {
-    client.release();
-    if (!isPoolClosed) {
-      try {
-        const deleteUserQuery = "DELETE FROM public.person WHERE username = $1";
-        await pool.query(deleteUserQuery, [newUser.username]);
-      } catch (error) {
-        console.error("Cleanup failed:", error);
-      }
-    }
-  });
+  // afterAll(async () => {
+    
+  //   client.release();
+  //   if (!isPoolClosed) {
+  //     try {
+  //       const deleteUserQuery = "DELETE FROM public.person WHERE username = $1";
+  //       await pool.query(deleteUserQuery, [newUser.username]);
+  //     } catch (error) {
+  //       console.error("Cleanup failed:", error);
+  //     }
+  //   }
+  // });
 
-  it("should successfully create a new user with valid data", async () => {
-    await client.query('BEGIN');
-    const result = await createUser(client, newUser);
+  // it("should successfully create a new user with valid data", async () => {
+  //   await client.query('BEGIN');
+  //   const result = await createUser(client, newUser);
 
-    expect(result.success).toBeTruthy();
-    expect(result.user).not.toBeNull();
-    expect(result.user.name).toBe(newUser.name);
+  //   expect(result.success).toBeTruthy();
+  //   expect(result.user).not.toBeNull();
+  //   expect(result.user.name).toBe(newUser.name);
 
-    await client.query('ROLLBACK'); 
-  });
+  //   await client.query('ROLLBACK'); 
+  // });
 });
 
 describe("findUserByUsername", () => {
@@ -60,23 +62,23 @@ describe("findUserByUsername", () => {
   });
 });
 
-describe("findUserByUsernameOrEmail", () => {
-  it("should return a User object when the user is found by name or by email", async () => {
+// describe("findUserByUsernameOrEmail", () => {
+//   it("should return a User object when the user is found by name or by email", async () => {
 
-    const user = await findUserByUsernameOrEmail("ArmandTodd", "armand@example.com");
+//     const user = await findUserByUsernameOrEmail("ArmandTodd", "armand@example.com");
 
-    expect(user).not.toBeNull();
-    expect(user).toBeInstanceOf(User);
-    expect(user.getName).toBe("Armand");
-  });
+//     expect(user).not.toBeNull();
+//     expect(user).toBeInstanceOf(User);
+//     expect(user.getName).toBe("Armand");
+//   });
 
-  it("should return a null object when the user is not found by name or by email", async () => {
+//   it("should return a null object when the user is not found by name or by email", async () => {
    
-    const user = await findUserByUsernameOrEmail("User1", "user1@example.com");
+//     const user = await findUserByUsernameOrEmail("User1", "user1@example.com");
 
-    expect(user).toBeNull();
-  });
-});
+//     expect(user).toBeNull();
+//   });
+// });
 
 
 afterAll(async () => {
